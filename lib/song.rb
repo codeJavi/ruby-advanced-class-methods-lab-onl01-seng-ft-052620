0b8eba8f42b493465647d365da0267206ef45edc
Javi Leyva <jvrleyva@gmail.com>
2020-06-04 16:06:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,28 @@
+class Song
+  attr_accessor :name, :artist_name
+  @@all = []
 
-  
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
   def self.create
-    song = Song.create 
+    song = Song.new
     song.save
     song
   end
 
-  def self.new_by_name("The Middle")
+  def self.new_by_name(song_name)
     song = self.new
     song.name = song_name
     song
   end
 
-  def self.create_by_name("The Middle")
+  def self.create_by_name(song_name)
     song = self.create
     song.name = song_name
     song
@@ -23,8 +33,7 @@
   end
 
   def self.find_or_create_by_name(song_name)
-    self.find_by_name(song_name) || 
-  self.create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
   end
 
   def self.alphabetical
@@ -56,3 +65,5 @@
   def self.destroy_all
     self.all.clear
   end
+
+end
